7aefd247d5e73dea70aa3be64d6978f0804f153f
ScoBoFoSho <ferrecisco@icloud.com>
2022-07-21 17:34:21 -0400

    homepage edit/getUserRepo works!

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -1,11 +1,29 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username!");
+  }
+  console.log(event);
+};
 // \testing javascript link to webpage
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+var getUserRepos = function (user) {
+  // github format for the api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    console.log(response);
     response.json().then(function (data) {
       console.log(data);
     });
-    console.log("inside", response);
   });
 };
-console.log("outside");
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
